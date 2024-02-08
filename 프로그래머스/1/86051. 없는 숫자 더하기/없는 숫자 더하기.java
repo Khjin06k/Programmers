0d24d4b3434cbcd424e3f3358064cf2f7594de5f
김희진 <95160590+Khjin06k@users.noreply.github.com>
2024-02-08 16:35:02 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        boolean[] check = new boolean[10];
+        for(int i = 0; i<numbers.length; i++){
+            check[numbers[i]] = true;
+        }
+        for(int i = 0; i<10; i++){
+            if(!check[i]) answer += i;
+        }
+        return answer;
+    }
+}
